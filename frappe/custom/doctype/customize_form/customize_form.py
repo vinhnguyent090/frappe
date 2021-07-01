df1fa0c1e9fa9360cd54483ecfac0e011cbf0b69
Vincent <vinhbk2000@gmail.com>
2021-07-01 18:28:34 +0700

    Vincent enable allow_on_submit

--- frappe/custom/doctype/customize_form/customize_form.py
+++ frappe/custom/doctype/customize_form/customize_form.py
@@ -193,12 +193,12 @@ class CustomizeForm(Document):
 		if prop == "fieldtype":
 			self.validate_fieldtype_change(df, meta_df[0].get(prop), df.get(prop))
 
-		elif prop == "allow_on_submit" and df.get(prop):
-			if not frappe.db.get_value("DocField",
-				{"parent": self.doc_type, "fieldname": df.fieldname}, "allow_on_submit"):
-				frappe.msgprint(_("Row {0}: Not allowed to enable Allow on Submit for standard fields")\
-					.format(df.idx))
-				return False
+		# elif prop == "allow_on_submit" and df.get(prop):
+		# 	if not frappe.db.get_value("DocField",
+		# 		{"parent": self.doc_type, "fieldname": df.fieldname}, "allow_on_submit"):
+		# 		frappe.msgprint(_("Row {0}: Not allowed to enable Allow on Submit for standard fields")\
+		# 			.format(df.idx))
+		# 		return False
 
 		elif prop == "reqd" and \
 			((frappe.db.get_value("DocField",
